76fa5a12f0c50d1f13e485bde3e58b19ea3e30f3
박지훈 <pjh@ziumks.com>
2024-07-15 10:02:16 +0900

    feat: 게시글 작성 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ContentAdd from "./components/ContentAdd";
 import RecipeList from "./components/RecipeList";
 
 function App() {
   return (
     <>
       <RecipeList />
+      <ContentAdd />
     </>
   );
 }
---
+++ src/components/ContentAdd.jsx
@@ -0,0 +1,17 @@
+function ContentAdd() {
+  return (
+    <div>
+      <div>
+        <label>제목</label>
+        <input />
+      </div>
+      <div>
+        <label>내용</label>
+        <textarea />
+      </div>
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default ContentAdd;
