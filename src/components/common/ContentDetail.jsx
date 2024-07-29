4e7770ff88832db26e68e55e2fbdc1124211f74d
박지훈 <pjh@ziumks.com>
2024-07-29 17:15:17 +0900

    .

--- src/components/common/ContentDetail.jsx
+++ src/components/common/ContentDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { ContentContext } from "../../contexts/ContentContext";
 import { GramContext } from "../../contexts/GramContext";
